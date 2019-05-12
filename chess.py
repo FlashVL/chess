846dac50bbb3646fe847f44d2ddbbdfd25557553
Alex <aleksandr-belousov-94@mail.ru>
2019-05-12 16:28:24 +1000

    0.0.5

--- chess.py
+++ chess.py
@@ -1,37 +1,50 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5.QtGui import QPainter, QColor, QBrush, QPen, QImage
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
-class Example(QWidget):
+
+class Example(QMainWindow):
 
     def __init__(self):
         super().__init__()
         self.initUI()
+        self.oldRect = 0
         self.flag = False
         self.x = 0
-        self.y = 0    
+        self.y = 0  
+        self.scene = QGraphicsScene()
+        self.graphics_view = QGraphicsView()
+        self.graphics_view.setScene(self.scene)
+        self.graphics_view.setSceneRect(0, 0, 800, 800)
+        self.setCentralWidget(self.graphics_view)        
+
     def initUI(self):    
-        self.setGeometry(300, 100, 800, 800)
+        self.setGeometry(300, 100, 802, 802)
         self.setWindowTitle('chess')
         self.show()
 
     def paintEvent(self, e):
-        qp = QPainter()
-        qp.begin(self)
-        self.drawRectangles(qp)
-        qp.end()
-        if self.flag:
-            self.paint = QPainter()
-            self.paint.begin(self)
-            self.paint.setPen(QPen(Qt.red,  2,))
-            self.paint.drawRect(self.x, self.y, 100, 100)
-            self.paint.end()
-            self.flag = False
+        for i in range(8): 
+            for j in range(8):   
+                if (8 - i + 1 + j) % 2 == 0:
+                    Rect = QGraphicsRectItem(i*100, j*100, 100, 100)
+                    Rect.setBrush(QColor(101, 67, 33))
 
-        painter = QPainter(self)
-        painter.drawImage(0, 100, QImage('/git/chess/figures/bP.png').scaled(100, 100))
-        painter.end()
+                else:
+                    Rect = QGraphicsRectItem(i*100, j*100, 100, 100)
+                    Rect.setBrush(QColor(255, 255, 255)) 
+                self.scene.addItem(Rect)                             
+        if self.flag:
+            if self.oldRect != 0:
+                del self.oldRect 
+            Rect = QGraphicsRectItem(self.x, self.y, 100, 100)
+            Rect.setPen(QPen(Qt.red,  2,))
+            self.scene.addItem(Rect)
+            self.flag = False
+            oldRect = Rect 
+
+
 
     def drawRectangles(self, qp):
 
@@ -49,12 +62,18 @@ class Example(QWidget):
                     qp.setBrush(QColor(255, 255, 255))
                     qp.drawRect(i*100, j*100, 100, 100)               
                     k = 0
+                self.scene.addItem(qp)
 
-    def mousePressEvent(self, event):      
+    def mousePressEvent(self, event):     
         self.x = int(event.pos().x() / 100) * 100
         self.y = int(event.pos().y() / 100) * 100
         self.flag = True
         self.update()   
+    
+    def pawn(self):
+        painter = QPainter(self)
+        painter.drawImage(0, 100, QImage('/git/chess/figures/bP.png').scaled(100, 100))
+        painter.end()
 
 app = QApplication(sys.argv)
 ex = Example()
