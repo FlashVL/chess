6618fb2f77d684bbf11ce373088c488ea4fb97f2
Alex <aleksandr-belousov-94@mail.ru>
2019-05-17 22:38:38 +1000

    0.0.7

--- chess.py
+++ chess.py
@@ -5,8 +5,9 @@ from PyQt5.QtCore import *
  
  
 class Figure(QGraphicsPixmapItem):
-    def __init__(self, parent = None):
+    def __init__(self, parent = None, color = None):
         QGraphicsPixmapItem.__init__(self, parent)
+        self.color = color
  
     def __str__(self):
         return 'figure'
@@ -16,8 +17,6 @@ class Example(QMainWindow):
         super().__init__()
         self.initUI()
         self.oldRect = 0
-        self.flag = False
-        self.first = True
         self.x = 0
         self.y = 0 
         self.oldx = 0
@@ -27,44 +26,41 @@ class Example(QMainWindow):
         self.graphics_view = QGraphicsView()
         self.graphics_view.setScene(self.scene)
         self.graphics_view.setSceneRect(0, 0, 800, 800)
-        self.setCentralWidget(self.graphics_view)        
+        self.setCentralWidget(self.graphics_view) 
+        self.DrawMap()        
  
     def initUI(self):    
         self.setGeometry(300, 100, 802, 802)
         self.setWindowTitle('chess')
-        self.show()
- 
+        self.show()      
+
+    def DrawMap(self):
+        for i in range(8): 
+            for j in range(8):   
+                if (8 - i + 1 + j) % 2 == 0:
+                    Rect = QGraphicsRectItem(i*100, j*100, 100, 100)
+                    Rect.setBrush(QColor(101, 67, 33))
+                else:
+                    Rect = QGraphicsRectItem(i*100, j*100, 100, 100)
+                    Rect.setBrush(QColor(255, 255, 255)) 
+                self.scene.addItem(Rect)   
+
+        pawnW1 = Figure(QPixmap('/git/chess/figures/bP.png').scaled(99, 99), 'black')
+        pawnW1.setOffset(300, 300)
+        self.scene.addItem(pawnW1)  
+
     def paintEvent(self, e):                   
-        if self.flag: #рисуем прямоугольник выделенную ячейку
-            if self.oldRect != 0:
-                self.scene.removeItem(self.oldRect)
-            Rect = QGraphicsRectItem(self.x, self.y, 100, 100)
-            Rect.setPen(QPen(Qt.red,  2,))
-            self.scene.addItem(Rect)
-            self.flag = False
-            self.oldRect = Rect
- 
-        else:
-            if self.first: #если первый запуск то рисуем шахматную доску  
-                for i in range(8): 
-                    for j in range(8):   
-                        if (8 - i + 1 + j) % 2 == 0:
-                            Rect = QGraphicsRectItem(i*100, j*100, 100, 100)
-                            Rect.setBrush(QColor(101, 67, 33))
- 
-                        else:
-                            Rect = QGraphicsRectItem(i*100, j*100, 100, 100)
-                            Rect.setBrush(QColor(255, 255, 255)) 
-                        self.scene.addItem(Rect)   
- 
-                pawnW1 = Figure(QPixmap('/git/chess/figures/bP.png').scaled(99, 99))
-                pawnW1.setOffset(300, 300)
-                self.scene.addItem(pawnW1)  
-                self.first = False
+        if self.oldRect != 0:
+            self.scene.removeItem(self.oldRect)
+        Rect = QGraphicsRectItem(self.x, self.y, 100, 100)
+        Rect.setPen(QPen(Qt.red,  2,))
+        self.scene.addItem(Rect)
+        self.flag = False
+        self.oldRect = Rect
+
     def mousePressEvent(self, event):     
         self.x = int(event.pos().x() / 100) * 100
         self.y = int(event.pos().y() / 100) * 100
-        self.flag = True
   
         serh = True
         it = self.graphics_view.items(self.x + 50, self.y + 50)
@@ -79,7 +75,6 @@ class Example(QMainWindow):
         self.oldx = self.x
         self.oldy = self.y 
         self.update() 
- 
  
 app = QApplication(sys.argv)
 ex = Example()
