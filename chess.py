90d51c2f51cde3dd3252d9f54fc28a0b4f779850
Alex <aleksandr-belousov-94@mail.ru>
2019-05-17 22:24:24 +1000

    0.0.7

--- chess.py
+++ chess.py
@@ -2,8 +2,15 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-
-
+ 
+ 
+class Figure(QGraphicsPixmapItem):
+    def __init__(self, parent = None):
+        QGraphicsPixmapItem.__init__(self, parent)
+ 
+    def __str__(self):
+        return 'figure'
+ 
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -21,14 +28,14 @@ class Example(QMainWindow):
         self.graphics_view.setScene(self.scene)
         self.graphics_view.setSceneRect(0, 0, 800, 800)
         self.setCentralWidget(self.graphics_view)        
-
+ 
     def initUI(self):    
         self.setGeometry(300, 100, 802, 802)
         self.setWindowTitle('chess')
         self.show()
-
+ 
     def paintEvent(self, e):                   
-        if self.flag:
+        if self.flag: #рисуем прямоугольник выделенную ячейку
             if self.oldRect != 0:
                 self.scene.removeItem(self.oldRect)
             Rect = QGraphicsRectItem(self.x, self.y, 100, 100)
@@ -36,35 +43,33 @@ class Example(QMainWindow):
             self.scene.addItem(Rect)
             self.flag = False
             self.oldRect = Rect
-
+ 
         else:
-            if self.first:   
+            if self.first: #если первый запуск то рисуем шахматную доску  
                 for i in range(8): 
                     for j in range(8):   
                         if (8 - i + 1 + j) % 2 == 0:
                             Rect = QGraphicsRectItem(i*100, j*100, 100, 100)
                             Rect.setBrush(QColor(101, 67, 33))
-
+ 
                         else:
                             Rect = QGraphicsRectItem(i*100, j*100, 100, 100)
                             Rect.setBrush(QColor(255, 255, 255)) 
                         self.scene.addItem(Rect)   
-        
-                pawnW1 = QGraphicsPixmapItem(QPixmap('/git/chess/figures/bP.png').scaled(99, 99))
-                self.scene.addItem(pawnW1)  
+ 
+                pawnW1 = Figure(QPixmap('/git/chess/figures/bP.png').scaled(99, 99))
                 pawnW1.setOffset(300, 300)
+                self.scene.addItem(pawnW1)  
                 self.first = False
     def mousePressEvent(self, event):     
         self.x = int(event.pos().x() / 100) * 100
         self.y = int(event.pos().y() / 100) * 100
         self.flag = True
-        
-   
-        
+  
         serh = True
         it = self.graphics_view.items(self.x + 50, self.y + 50)
         for k in it:
-            if str(type(k)) =="<class 'PyQt5.QtWidgets.QGraphicsPixmapItem'>":
+            if str(k) == 'figure':
                 self.oldFigures = k
                 serh = False
         if serh and self.oldFigures != None:
@@ -74,8 +79,8 @@ class Example(QMainWindow):
         self.oldx = self.x
         self.oldy = self.y 
         self.update() 
-
-
+ 
+ 
 app = QApplication(sys.argv)
 ex = Example()
 sys.exit(app.exec_())
